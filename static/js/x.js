3dd850695c3bda48641ad6c65455e8eabb839ba1
Cenk Alti <cenkalti@gmail.com>
2015-04-24 18:35:10 +0300

    reconnect sockjs

--- static/js/x.js
+++ static/js/x.js
@@ -1,5 +1,6 @@
 angular.module('main', [])
-    .controller('MainController', function($scope, sockjs) {
+    .controller('MainController', function($scope, sock) {
+        $scope.loggedIn = false;
         $scope.name = null;
         $scope.contacts = [{
             name: "cenk"
@@ -13,18 +14,29 @@ angular.module('main', [])
         }];
         $scope.login = function() {
             $scope.name = prompt("Enter your name:");
-            sockjs.send(JSON.stringify({
+            localStorage.name = $scope.name;
+            sock.get().send(JSON.stringify({
                 type: "login",
                 name: $scope.name
             }));
+            $scope.loggedIn = true;
+        }
+        $scope.logout = function() {
+            $scope.name = null;
+            $scope.loggedIn = false;
+            delete localStorage.name;
         }
         $scope.newWindow = function(to) {
             $scope.windows.push({
                 to: to
             });
         }
+        if (localStorage.name) {
+            $scope.name = localStorage.name;
+            $scope.loggedIn = true;
+        }
     })
-    .controller('WindowController', function($scope, sockjs) {
+    .controller('WindowController', function($scope, sock) {
         $scope.to = $scope.window.to;
         $scope.text = "";
         $scope.messages = [];
@@ -34,7 +46,7 @@ angular.module('main', [])
             $scope.messages.push({
                 body: text
             });
-            sockjs.send(JSON.stringify({
+            sock.get().send(JSON.stringify({
                 type: "chat",
                 from: $scope.name,
                 to: $scope.to,
@@ -54,16 +66,35 @@ angular.module('main', [])
             });
         }
     })
-    .factory('sockjs', function() {
-        var sock = new SockJS('/sockjs/sock');
-        sock.onopen = function() {
-            console.log('opened sockjs session');
+    .factory('sock', function() {
+        var sock = null;
+        var retryInterval = null;
+
+        function newSocket() {
+            sock = new SockJS('/sockjs/sock');
+            sock.onopen = function() {
+                clearInterval(retryInterval);
+                console.log('opened sockjs session');
+                if (localStorage.name) {
+                    sock.send(JSON.stringify({
+                        type: "login",
+                        name: localStorage.name
+                    }));
+                }
+            };
+            sock.onmessage = function(e) {
+                console.log('received message', e.data);
+            };
+            sock.onclose = function() {
+                sock = null;
+                console.log('closed sockjs session');
+                retryInterval = setTimeout(newSocket, 2000)
+            };
+        }
+        newSocket();
+        return {
+            get: function() {
+                return sock;
+            }
         };
-        sock.onmessage = function(e) {
-            console.log('received message', e.data);
-        };
-        sock.onclose = function() {
-            console.log('closed sockjs session');
-        };
-        return sock;
     });
