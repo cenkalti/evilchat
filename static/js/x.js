096af54c10ff2a694421729567ea595ba3397fca
Cenk Alti <cenkalti@gmail.com>
2015-04-24 17:22:08 +0300

    simpler enter

--- static/js/x.js
+++ static/js/x.js
@@ -43,18 +43,16 @@ angular.module('main', [])
         };
     })
     .directive('enter', function() {
-        return {
-            link: function(scope, element, attrs) {
-                element.bind("keydown keypress", function(event) {
-                    if (event.which === 13) {
-                        scope.$apply(function() {
-                            scope.$eval(attrs.enter);
-                        });
-                        event.preventDefault();
-                    }
-                });
-            }
-        };
+        return function(scope, element, attrs) {
+            element.bind("keypress", function(event) {
+                if (event.which === 13) {
+                    scope.$apply(function() {
+                        scope.$eval(attrs.enter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        }
     })
     .factory('sockjs', function() {
         var sock = new SockJS('/sockjs/sock');
